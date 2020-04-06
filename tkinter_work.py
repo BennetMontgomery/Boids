eb0756f4ce34281dc878ebb9d02e4d85ee4cb2f9
shreyanshanand <s.anand@queensu.ca>
2020-04-06 17:08:07 -0400

    finished the gui

--- tkinter_work.py
+++ tkinter_work.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import boids
 import random
+import time
 
 grid = []
 grid_size = 100
@@ -8,11 +9,11 @@ grid_size = 100
 
 def init_boids():
     boid_list = []
-    for i in range(10):
+    for i in range(150):
         vals = []
         pos = []
         for j in range(2):
-            pos.append(random.randrange(200, 300))
+            pos.append(random.randrange(305, 455))
         for j in range(2):
             vals.append(random.randrange(-1, 1))
         boid_list.append(boids.Boid((pos[0], pos[1]), (vals[0], vals[1])))
@@ -25,33 +26,25 @@ def main():
     root = Tk()
     title = Label(root, text='Boids Simulation')
     title.pack()
-    img = PhotoImage(file='arrow.png')
+    img = PhotoImage(file='tweet.png')
 
     tweet_list = []
     c = Canvas(master=root, width=1000, height=800)
     for i in range(len(boid_list)):
         tweet_list.append(c.create_image(boid_list[i].getPosition()[0], boid_list[i].getPosition()[1], anchor=NW, image=img))
-
-    def keypress(event):
-        if event.char== 'a':
-            print("h")
-            print(boid_list[0].getPosition())
-            boids.move_all_boids_to_new_positions(boid_list)
-            print(boid_list[0].getPosition())
-            for i in range(len(boid_list)):
-                c.move(tweet_list[i], boid_list[i].getVelocity()[0], boid_list[i].getVelocity()[1])
     
     c.pack()
 
-    # def move_tweets():
-    #     boids.move_all_boids_to_new_positions(boid_list)
-    #     for i in range(len(boid_list)):
-    #         c.move(tweet_list[i], boid_list[i].getPosition()[0], boid_list[i].getPosition()[1])
-    #     c.after(10, move_tweets())
-    # move_tweets()
-    # seems to have an issue?
 
-    root.bind("<Key>", keypress)
+    while True:
+        boids.move_all_boids_to_new_positions(boid_list)
+        time.sleep(0.025)
+        for i in range(len(boid_list)):
+            c.move(tweet_list[i], boid_list[i].getVelocity()[0], boid_list[i].getVelocity()[1])
+
+        c.update()
+
+    # root.bind("<Key>", keypress)
 
     root.mainloop()
 
